0748692c3e4f405cd030182f1c8d7af60a59a05d
skyland31 <suttipong.kay.10@gmail.com>
2022-05-23 17:36:07 +0700

    Connect database oracle.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/sayHi")
+	public String sayHi(){
+		return "Hello1";
+	}
+
+	@GetMapping("/sayHi2")
+	public String sayHi2(){
+		return "Hello2";
 	}
 
 }
